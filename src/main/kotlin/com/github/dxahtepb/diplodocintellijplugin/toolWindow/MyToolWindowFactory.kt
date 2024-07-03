02b09f2decc4d953ea83319b42fc83135fee83a7
GitHub Action <action@github.com>
2024-07-03 15:59:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/dxahtepb/diplodocintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/dxahtepb/diplodocintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.dxahtepb.diplodocintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.dxahtepb.diplodocintellijplugin.MyBundle
+import com.github.dxahtepb.diplodocintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 
